4f311a2c75f148e285a18d62f9eca3b87001f86c
Rhendrik Rian <rhendrik20@gmail.com>
2024-06-08 13:36:12 +0700

    second

--- routes/Items.js
+++ routes/Items.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-// Item Model
 const Item = require('../models/Item');
 
-// @route   GET api/items
-// @desc    Get All Items
-// @access  Public
+//get
 router.get('/', (req, res) => {
     Item.find()
         .sort({ date: -1 })
         .then(items => res.json(items));
 });
 
-// @route   POST api/items
-// @desc    Create An Item
-// @access  Public
+//add
 router.post('/', (req, res) => {
     const newItem = new Item({
         name: req.body.name
@@ -24,18 +19,13 @@ router.post('/', (req, res) => {
     newItem.save().then(item => res.json(item));
 });
 
-// @route   DELETE api/items/:id
-// @desc    Delete An Item
-// @access  Public
+//delete
 router.delete('/:id', (req, res) => {
     Item.findById(req.params.id)
         .then(item => item.remove().then(() => res.json({ success: true })))
         .catch(err => res.status(404).json({ success: false }));
 });
-
-// @route   PUT api/items/:id
-// @desc    Update An Item
-// @access  Public
+//update
 router.put('/:id', (req, res) => {
     Item.findById(req.params.id)
         .then(item => {
